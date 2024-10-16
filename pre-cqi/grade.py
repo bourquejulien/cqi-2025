f2c97c7432a063642ddee4e0394581a0f7578188
Julien Bourque <julien@brqu.me>
2024-10-15 21:11:25 -0400

    Improved grading script

--- pre-cqi/grade.py
+++ pre-cqi/grade.py
@@ -8,35 +8,72 @@ Installation:
 - pip install docker
 '''
 
-import socket
-import sys
-import requests
-import time
 import signal
-import docker
+import time
+import requests
+import sys
+import socket
+import uuid
+import subprocess
 
-from docker import DockerClient
-from docker.models.networks import Network
-from docker.models.containers import Container
+
+def imports() -> None:
+    global docker, DockerClient, Container, Network
+    import docker
+    from docker import DockerClient
+    from docker.models.containers import Container
+    from docker.models.networks import Network
+
+
+def install_dependencies(dependencies: list[str]) -> bool:
+    print(f"Failed to load dependencies: {dependencies}")
+    should_install = input(
+        "Do you want to install them in the current environnement (Y, N)? \n").lower().startswith("y")
+    if not should_install:
+        return False
+
+    try:
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", " ".join(dependencies)])
+    except:
+        print("Unable to install dependencies, did you install pip (https://pip.pypa.io/en/stable/installation/)?")
+        return False
+
+    return True
+
+
+try:
+    imports()
+except:
+    if not install_dependencies(["docker"]):
+        print("Install the required dependencies to continue")
+        exit(1)
+    imports()
+
 
 GRADING_IMAGE_NAME = "brqu/pre-cqi-prog-2025:latest"
+GRADER_NAME_BASE = "grader"
+TEAM_NAME_BASE = "team"
+NETWORK_NAME_BASE = "grader_network"
 RUN_COUNT = 10
 GAME_TIMEOUT = 30
 STOP_TIMEOUT = 5
 
 
-def get_port() -> int:
+should_stop = False
+
+
+def signal_handler(sig, frame) -> None:
+    global should_stop
+    print("Stopping...")
+    should_stop = True
+
+
+def get_available_port() -> int:
     with socket.socket() as sock:
         sock.bind(("localhost", 0))
         port = sock.getsockname()[1]
         return port
-
-
-should_stop = False
-def signal_handler(sig, frame):
-    global should_stop
-    print("Stopping...")
-    should_stop = True
 
 
 class Grader:
@@ -45,36 +82,37 @@ class Grader:
     _grader_port: int
     _network_name: str
     _grading_container_name: str
+    _team_container_name: str
 
     _network: Network | None
     _grading_container: Container | None
     _team_container: Container | None
 
     def __init__(self, client: DockerClient, team_image_name: str) -> None:
+        run_id = uuid.uuid4().hex[:5]
+
         self._client = client
         self._team_image_name = team_image_name
-        self._grader_port = get_port()
+        self._grader_port = get_available_port()
         self._grading_container = None
         self._team_container = None
-        self._network_name = "grader_network"
-        self._grading_container_name = "grader"
+        self._network_name = f"{NETWORK_NAME_BASE}_{run_id}"
+        self._grading_container_name = f"{GRADER_NAME_BASE}_{run_id}"
+        self._team_container_name = f"{TEAM_NAME_BASE}_{run_id}"
 
-    def prepare(self):
+    def prepare(self) -> bool:
+        print("Pulling containers...")
         self._client.images.pull(GRADING_IMAGE_NAME)
-        self._client.images.pull(self._team_image_name)
 
-        for container in self._client.containers.list(all=True):
-            if container.name == self._grading_container_name:
-                container.remove(force=True)
-                break
+        try:
+            self._client.images.pull(self._team_image_name)
+        except Exception as e:
+            print(f"Unable to pull {self._team_image_name}: {e}")
+            return False
 
-        for network in self._client.networks.list():
-            if network.name == self._network_name:
-                for container in network.containers:
-                    network.disconnect(container, force=True)
-                network.remove()
-                break
+        self._cleanup()
 
+        print("Starting grader...")
         self._network = self._client.networks.create(
             self._network_name, driver="bridge")
         self._grading_container = self._client.containers.run(GRADING_IMAGE_NAME, name=self._grading_container_name,
@@ -86,7 +124,22 @@ class Grader:
         if not response.ok and not response.content.startswith(b"No game available"):
             raise Exception("Failed to connect to grading server")
 
-    def stop(self):
+        print("Grader started successfully")
+        return True
+
+    def _cleanup(self):
+        container: Container
+        for container in self._client.containers.list(all=True):
+            if container.name.startswith((GRADER_NAME_BASE, TEAM_NAME_BASE)):
+                container.remove(force=True)
+
+        for network in self._client.networks.list():
+            if network.name.startswith(NETWORK_NAME_BASE):
+                for container in network.containers:
+                    network.disconnect(container, force=True)
+                network.remove()
+
+    def stop(self) -> None:
         self.reset()
         self._grading_container.stop(timeout=STOP_TIMEOUT)
         self._network.remove()
@@ -101,7 +154,12 @@ class Grader:
             print("Bot timed out")
             return None
 
-        response = requests.get(f"http://localhost:{self._grader_port}/status")
+        try:
+            response = requests.get(
+                f"http://localhost:{self._grader_port}/status")
+        except:
+            print("Unable to access grader")
+            return None
 
         if not response.ok:
             print("No game status available")
@@ -113,7 +171,7 @@ class Grader:
 
         return game_response["score"]
 
-    def reset(self):
+    def reset(self) -> None:
         if self._team_container is None:
             return
 
@@ -125,9 +183,19 @@ class Grader:
         self._team_container = None
 
 
-def main():
-    signal.signal(signal.SIGINT, signal_handler)
-    client = docker.from_env()
+def main() -> None:
+    client: DockerClient
+
+    try:
+        client = docker.from_env()
+    except:
+        noob_instructions = \
+            "Unable to access Docker. Is docker installed and running (can you run \"docker run hello-world\")?\n" \
+            "If you are on Linux and Docker is install, you probably need to allow Docker to be accessed from your user (https://docs.docker.com/engine/install/linux-postinstall/). You can also run this script as root, but thats NOT RECOMMENDED.\n" \
+            "Else you need to install Docker: https://docs.docker.com/get-started/get-docker/.\n" \
+            "Note: If you are using Windows, the WSL 2 Docker installation method is preferred."
+        print(noob_instructions)
+        exit(1)
 
     team_image_name: str
     if len(sys.argv) < 2:
@@ -136,8 +204,10 @@ def main():
     else:
         team_image_name = sys.argv[1]
 
+    signal.signal(signal.SIGINT, signal_handler)
     grader = Grader(client, team_image_name)
-    grader.prepare()
+    if not grader.prepare():
+        exit(1)
 
     total_score = 0
     for i in range(RUN_COUNT):
