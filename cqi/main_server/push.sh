f8b6bd3fe713644c9f945e26ef25c2fa6c856318
Julien Bourque <julien@brqu.me>
2024-12-07 22:52:29 -0500

    Add publish main server

--- cqi/main_server/push.sh
+++ cqi/main_server/push.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-DOCKER_HUB_IMAGE="brqu/server"
+DOCKER_IMAGE="ghcr.io/bourquejulien/cqi-2024-gamer-server:latest"
 BUILDX_CONTEXT="cqi"
 
 docker buildx create --use --name $BUILDX_CONTEXT
-docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_HUB_IMAGE --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_IMAGE --push .
 docker buildx rm $BUILDX_CONTEXT
