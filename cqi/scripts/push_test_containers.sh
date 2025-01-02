bbb30f388bd16e7195290c4fe482210dbdae73cd
Julien Bourque <julien@brqu.me>
2025-01-01 21:12:35 -0500

    Added push script

---
+++ cqi/scripts/push_test_containers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME="ghcr.io/bourquejulien/cqi-2024-easy-bot:latest"
+AWS_ECR_NAME="481665101132.dkr.ecr.us-east-1.amazonaws.com"
+
+NAMES=$(aws secretsmanager get-secret-value \
+    --secret-id user_secrets \
+    --query "SecretString" \
+    --output text \
+    | jq "keys[]" -r)
+
+docker pull $IMAGE_NAME
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$AWS_ECR_NAME"
+
+for NAME in $NAMES; do
+    echo "Pushing $NAME"
+    NAME="${AWS_ECR_NAME}/${NAME}"
+    docker tag $IMAGE_NAME "$NAME"
+    docker push "$NAME"
+done
