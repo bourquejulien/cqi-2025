ba2d60145b916b060396ca92a753e991c3e62335
Julien Bourque <julien@brqu.me>
2025-01-08 23:26:52 -0500

    Improved push test script

--- cqi/scripts/push_test_containers.sh
+++ cqi/scripts/push_test_containers.sh
@@ -3,7 +3,7 @@
 set -e
 
 IMAGE_NAME_EASY="ghcr.io/bourquejulien/cqi-2025-easy-bot:latest"
-IMAGE_NAME_MEDIUM="ghcr.io/bourquejulien/cqi-2025-easy-medium:latest"
+IMAGE_NAME_MEDIUM="ghcr.io/bourquejulien/cqi-2025-medium-bot:latest"
 
 AWS_ECR_NAME="481665101132.dkr.ecr.us-east-1.amazonaws.com"
 
@@ -13,12 +13,23 @@ NAMES=$(aws secretsmanager get-secret-value \
     --output text \
     | jq "keys[]" -r)
 
-docker pull $IMAGE_NAME
+docker pull $IMAGE_NAME_EASY
+docker pull $IMAGE_NAME_MEDIUM
+
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$AWS_ECR_NAME"
 
+i=0
 for NAME in $NAMES; do
-    echo "Pushing $NAME"
+    if (( $((i % 2)) == 0 )); then
+        IMAGE_NAME=$IMAGE_NAME_EASY
+    else
+        IMAGE_NAME=$IMAGE_NAME_MEDIUM
+    fi
+
+    i=$((i + 1))
+
     NAME="${AWS_ECR_NAME}/${NAME}"
+    echo "Image: $IMAGE_NAME to: $NAME"
     docker tag $IMAGE_NAME "$NAME"
     docker push "$NAME"
 done
